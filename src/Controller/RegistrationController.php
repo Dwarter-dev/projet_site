c5745cdd6de5320be83a8554516fbbd55519bea9
Steven Da Costa <dacostasteven91@gmail.com>
2022-06-07 14:00:17 +0200

    3. Modifications du register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -47,7 +47,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('dacostasteven91@gmail.com', 'MonSite'))
+                    ->from(new Address('dacostasteven91@gmail.com', 'MonSite')) // A changer
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
@@ -77,8 +77,8 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Votre adresse a été bien vérifiée.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('main');
     }
 }
