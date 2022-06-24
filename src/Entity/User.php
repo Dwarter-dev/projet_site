814a7d1546d0358e48a9af2fd354f69faf8247cb
Steven Da Costa <dacostasteven91@gmail.com>
2022-06-24 09:02:04 +0200

    Léger changement et intégration d'un message de succès/fail

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,9 +11,8 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-/**
- * #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
- */
+#[UniqueEntity(fields: ['email'], message: 'Il y a déjà un compte qui utilise cette adresse mail !')]
+
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
